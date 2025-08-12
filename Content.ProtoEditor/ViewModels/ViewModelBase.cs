da9d9880dfb93c3e16ea4b47cb48cf5335a7f878
BarryNorfolk <barrynorfolkman@protonmail.com>
2025-08-12 18:58:44 +0200

    Initial commit of new avalonia project

---
+++ Content.ProtoEditor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Content.ProtoEditor.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Content.ProtoEditor/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Content.ProtoEditor.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
