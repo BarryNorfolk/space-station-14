0739391decfa741fb696822549b349fa30adbb94
BarryNorfolk <barrynorfolkman@protonmail.com>
2025-08-26 18:36:44 +0200

    Update the PrototypeKind to be simpler

--- Content.ProtoEditor/Models/PrototypeKind.cs
+++ Content.ProtoEditor/Models/PrototypeKind.cs
@@ -14,27 +14,17 @@ public sealed class PrototypeKind(Type kind)
     /// <summary>
     /// The actual type for the IPrototype, as determined by the IPrototypeManager.
     /// </summary>
-    private readonly Type _kind = kind;
+    public readonly Type Kind = kind;
 
     /// <summary>
     /// Full name of the prototype kind.
     /// E.g. "Content.Shared.Store.ListingPrototype".
     /// </summary>
-    public string Name => _kind.FullName ?? _kind.ToString();
+    public string Name => Kind.FullName ?? Kind.ToString();
 
     /// <summary>
     /// Simple short name for viewing in UI.
     /// E.g. "Content.Shared.Store.ListingPrototype" becomes "ListingPrototype".
     /// </summary>
-    public string ShortName => _kind.ShortDisplayName();
-
-    /// <summary>
-    /// Equality checking between two prototype kinds by comparing their actual Type.
-    /// </summary>
-    /// <param name="lhs">The 'Other' PrototypeKind object to compare with.</param>
-    /// <returns>True if the types match, otherwise false.</returns>
-    public bool Equals(PrototypeKind lhs)
-    {
-        return _kind == lhs._kind;
-    }
+    public string ShortName => Kind.ShortDisplayName();
 }
