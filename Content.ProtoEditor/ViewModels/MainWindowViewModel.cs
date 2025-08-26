65dfe6c631a17011af7af88ba951fbcc8df8946d
BarryNorfolk <barrynorfolkman@protonmail.com>
2025-08-26 18:48:55 +0200

    Update main window to use new views

--- Content.ProtoEditor/ViewModels/MainWindowViewModel.cs
+++ Content.ProtoEditor/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,46 @@
-﻿namespace Content.ProtoEditor.ViewModels;
+﻿using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Content.ProtoEditor.Services;
 
-public partial class MainWindowViewModel : ViewModelBase
+namespace Content.ProtoEditor.ViewModels;
+
+public sealed partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    /// <summary>
+    /// Simple text for showing some information on what the program is doing right now
+    ///
+    /// TODO: This could be a more impactful "State" update system, we should make something
+    /// that shows users what is going on. In case there are any long lived processes occuring.
+    /// </summary>
+    [ObservableProperty]
+    private string _state = "Starting up";
+
+    /// <summary>
+    /// Stored reference to the PrototypeManager provided by the ProtoEditor.
+    /// </summary>
+    private readonly AssemblyProvider _assemblyProvider;
+
+    /// <summary>
+    /// Task for handling Initialization of this model view.
+    /// Can be await'ed on by other dependents to ensure required properties are setup.
+    /// </summary>
+    public Task Initialization { get; private set; }
+
+    public MainWindowViewModel(AssemblyProvider assemblyProvider)
+    {
+        _assemblyProvider = assemblyProvider;
+
+        Initialization = InitializeAsync();
+    }
+
+    /// <summary>
+    /// Async Initialization of this ViewModel.
+    /// </summary>
+    /// <returns>The async task for the initialization.</returns>
+    private async Task InitializeAsync()
+    {
+        await _assemblyProvider.Initialization;
+
+        State = "Assemblies Loaded.";
+    }
 }
